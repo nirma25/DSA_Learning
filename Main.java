86078511af624a8d882b488f9376899d69610dc1
nirma25 <nirmaab025@gmail.com>
2026-06-19 22:56:19 +0530

    updates

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
-import java.util.ArrayList;
-
 public class Main {
         public static void main(String[] args) {
-            ArrayList<Integer> list = new ArrayList<>();
-            list.add(10);
-            list.add(20);
-            list.add(30);
-            list.remove(0);
-            list.indexOf(20);
-            list.contains(20);
-            list.size();
-            list.toArray();
-            System.out.println(list);
+            
     }
 }
